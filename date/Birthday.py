34feed262831ce1a804d2b9530e6c7f2713d5c79
hyeg0121 <hyeg0121@naver.com>
2023-02-16 23:45:10 +0900

    datetime라이브러리를 이용해서 태어난 요일을 구하는 예제 작성

---
+++ date/Birthday.py
@@ -0,0 +1,30 @@
+import datetime
+
+#사용자 입력 받기
+year = input("태어난 해는 언제인가요?")
+year = int(year)
+month = input("태어난 달은 언제인가요?")
+month = int(month)
+day = input("태어난 날은 언제인가요?")
+day = int(day)
+
+#날짜 만들기
+bday = datetime.datetime(year, month, day)
+
+#결과 출력하기
+if bday.weekday() == 6:
+    dow = "일"
+elif bday.weekday() == 0:
+    dow = "월"
+elif bday.weekday() == 1:
+    dow = "화"
+elif bday.weekday() == 2:
+    dow = "수"
+elif bday.weekday() == 3:
+    dow = "목"
+elif bday.weekday() == 4:
+    dow = "금"
+elif bday.weekday() == 5:
+    dow = "토"
+
+print("태어난 요일은", dow,"요일 입니다.")
